f9b9e9de3a296d9d665fc5291b7c2e62ed9bb4b7
Zhang UK <spquyt@gmail.com>
2018-05-05 22:09:23 +0700

    this shit is not gud

--- SNAKE.cpp
+++ SNAKE.cpp
@@ -28,13 +28,37 @@ void display(){
 	}
 }
 
-void move(char dir, int pos_x_next, int pos_y_next){
+void moveDown(){
 	Cor temp = snake.head;
-	
+	map[snake.tail.pos_x][snake.tail.pos_y] = ' ';
+	map[snake.head.pos_x + 1][snake.head.pos_y + 0] = 'O';
+	if (snake.tail.pos_y == temp.pos_y){
+		snake.head.pos_x++;
+		snake.tail.pos_x++;
+	}
+	else{
+		snake.head.pos_x++;
+		snake.tail.pos_y++;
+	}
+}
+
+void moveRight(){
+	Cor temp = snake.head;
+	map[snake.tail.pos_x][snake.tail.pos_y] = ' ';
+	map[snake.head.pos_x + 0][snake.head.pos_y + 1] = 'O';
+	if (snake.tail.pos_x == temp.pos_x){
+		snake.head.pos_y++;
+		snake.tail.pos_y++;
+	}
+	else{
+		snake.head.pos_y++;
+		snake.tail.pos_x++;
+	}
 }
 
 int main(){
-	for (int i = 0; i < num; i++){
+	
+	for (int i = 0; i < num; i++){       // make a new map
 		for (int j = 0; j < num; j++){
 			map[i][j] = '#';
 		}
@@ -46,14 +70,22 @@ int main(){
 	}
 	
 	
-	for (int j = 1; j <= len; j++){
+	for (int j = 1; j <= len; j++){  // make a new snake
 		map[3][j] = 'O';
 	}
-	snake.head.pos_x = 3; snake.head.pos_y = 6;
-	snake.tail.pos_x = snake.head.pos_x; snake.tail.pos_y = snake.head.pos_y - len;
+	snake.head.pos_x = 3; snake.head.pos_y = 5;
+	snake.tail.pos_x = snake.head.pos_x; snake.tail.pos_y = snake.head.pos_y - len + 1;
 	
-	while(1){
-		
-	}
 	display();
+	sleep(1);
+	while(snake.head.pos_y != num-1 && snake.head.pos_y != 0 && snake.head.pos_x != num-1 && snake.head.pos_y != 0){
+		moveRight();
+		system ("cls");
+		display();
+		sleep(3);
+		moveDown();
+		system ("cls");
+		display();
+		sleep(3);		
+	}
 }
