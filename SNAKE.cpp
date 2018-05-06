2714823b162be709050b2302443db22e63cb3b83
Zhang UK <spquyt@gmail.com>
2018-05-06 20:04:57 +0700

    gud breakthrough again

--- SNAKE.cpp
+++ SNAKE.cpp
@@ -3,11 +3,8 @@
 #include <string.h>
 #include <windows.h>
 #include <unistd.h>
-<<<<<<< HEAD
-#include <iostream>
-=======
->>>>>>> 1ddfa2d4c430a9313b5fe4c6b4eba8fd45644859
 #include <conio.h>
+#include <time.h>
 
 #define num 13
 #define len 3
@@ -23,6 +20,36 @@ struct Node{
 	Node *prev;
 };
 
+int check_body(int row, int col){		//check if a part of snake's body is there?
+	if (map[row][col] == 'O' || map[row][col] == 'X'){
+		return true;
+	}
+	return false;
+}
+
+void gen_food(){
+	int row, col;
+	srand(time(NULL));
+	while(check_body(row,col) == true){
+		row = rand() % 11 + 1;
+		col = rand() % 11 + 1;
+	}
+	map[row][col] ='.';	
+}
+
+void init_map(){
+	for (int i = 0; i < num; i++){
+		for (int j = 0; j < num; j++){
+			map[i][j] = '#';
+		}
+	}
+	for (int i = 1; i < num-1; i++){
+		for (int j = 1; j < num-1; j++){
+			map[i][j] = ' ';
+		}
+	}
+	gen_food();
+}
 
 void display(){
 	system ("cls");
@@ -32,30 +59,25 @@ void display(){
 		}
 		printf("\n");
 	}
-	sleep(1);
+	_sleep(100); // delaying time
 }
 
-void delete_last(Node *first){
-	Node *pre = first;
+void delete_last(Node *first){   //delete the tail
+	Node *aft = first;
 	Node *to_del =first;
 	while(to_del->prev != NULL){
-		pre = to_del;
+		aft = to_del;
 		to_del = to_del->prev;
 	}
-<<<<<<< HEAD
-	system ("cls");
-	display();
-	sleep(1);
-=======
 	map[to_del->cor.pos_x][to_del->cor.pos_y] = ' ';
-	pre->prev = NULL;
+	aft->prev = NULL;
 	free(to_del);
->>>>>>> 1ddfa2d4c430a9313b5fe4c6b4eba8fd45644859
+
 }
 
 void init_snake(Node *first){
 	Node *p = first;
-	for (int i = 6; i > 3; i--){
+	for (int i = 6; i > 5; i--){
 		Node *temp = new Node();
 		p->prev = temp;
 		temp->cor.pos_y = i;
@@ -70,85 +92,79 @@ void update_snake(Node *first){
 	while (p != NULL){
 		if (p == first){
 			p = p->prev;
+			map[p->cor.pos_x][p->cor.pos_y] = 'X';    //head of the snake
+			p = p->prev;
 		}
 		else{
-			map[p->cor.pos_x][p->cor.pos_y] = 'O';
+			map[p->cor.pos_x][p->cor.pos_y] = 'O';	  //body of the snake
 			p = p->prev;	
 		}
 	}
 	system ("cls");
 	display();
-	sleep(1);
 }
 
 void move(Node *first, int dx, int dy){
-	delete_last(first);
-	Node *temp = new Node();
+	Node *temp = new Node();                          //
 	temp->cor.pos_x = first->prev->cor.pos_x + dx;
 	temp->cor.pos_y = first->prev->cor.pos_y + dy;
 	temp->prev = first->prev;
-	first->prev = temp;
+	first->prev = temp;                                // move the head
+	
+	if (map[temp->cor.pos_x][temp->cor.pos_y] != '.'){
+		delete_last(first);  							// delete the tail
+	}
+	else{
+		gen_food();
+	}
+	
 	update_snake(first);
 	display();	
 }
 
+void running(Node *&head){								// create a reference
+	char dir;
+	while(head->prev->cor.pos_y != num-1 && head->prev->cor.pos_y != 0 && head->prev->cor.pos_x != num-1 && head->prev->cor.pos_x != 0){
+			if (kbhit()){
+				if (GetAsyncKeyState(VK_UP)){
+					dir = 'w';
+				}
+				else if (GetAsyncKeyState(VK_DOWN)){
+					dir = 's';
+				}
+				else if (GetAsyncKeyState(VK_RIGHT)){
+					dir = 'd';
+				}
+				else if (GetAsyncKeyState(VK_LEFT)){
+					dir = 'a';
+				}
+			}
+			switch (dir){
+				case 'a':
+					move(head,0,-1);break;
+				case 'd':
+					move(head,0,1); break;
+				case 'w':
+					move(head,-1,0); break;
+				case 's':
+					move (head,1,0); break;
+			}
+	}
+
+}
+
 int main(){
-<<<<<<< HEAD
-	char key;
-=======
+
+	char dir;
+
 	Node *head = new Node();
 	head->prev = NULL;
-	head->cor.pos_x = head->cor.pos_y = 0;
->>>>>>> 1ddfa2d4c430a9313b5fe4c6b4eba8fd45644859
+
 	
-	for (int i = 0; i < num; i++){
-		for (int j = 0; j < num; j++){
-			map[i][j] = '#';
-		}
-	}
-	for (int i = 1; i < num-1; i++){
-		for (int j = 1; j < num-1; j++){
-			map[i][j] = ' ';
-		}
-	}
-	
-	init_snake(head);
-	
+	init_map();	
+	init_snake(head);	
 	update_snake(head);
 	display();
-<<<<<<< HEAD
-	sleep(1);
-	while(snake.head.pos_y != num-1 && snake.head.pos_y != 0 && snake.head.pos_x != num-1 && snake.head.pos_x != 0){
-		if (kbhit()){
-			if (GetAsyncKeyState(VK_UP)){
-				snake.dir = 'w';
-			}
-			else if (GetAsyncKeyState(VK_DOWN)){
-				snake.dir = 's';
-			}
-			else if (GetAsyncKeyState(VK_RIGHT)){
-				snake.dir = 'd';
-			}
-			else{
-				snake.dir = 'a';
-			}
-		}
-		switch (snake.dir){
-			case 'd':
-				moveRight(); break;
-			case 's':
-				moveDown(); break;
+	running(head);
 
-		}
-	}
-=======
-	while(1){
-		move(head,0,1);
-		move(head,1,0);
-	}	
-
-		
-	
-
->>>>>>> 1ddfa2d4c430a9313b5fe4c6b4eba8fd45644859
 }
